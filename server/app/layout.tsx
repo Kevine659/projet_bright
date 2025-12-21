a81d3c2b8580334ba8ddce221111fbeb09c823b7
ryantido34 <ryantido34@gmail.com>
2025-12-21 10:09:47 +0100

    doc(metadata): update metada from updated Rootlyout

--- server/app/layout.tsx
+++ server/app/layout.tsx
@@ -1,8 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Bright Consulting",
+  description: "Bright Consulting",
+};
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
